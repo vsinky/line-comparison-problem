6ac0993548a5e130ed3b68cc045b5abbf8af0f84
vsinky <sinkykumariverma@gmail.com>
2021-11-08 22:44:38 +0530

    welcome to line comparison computation

---
+++ src/com/bridgelabz/line_comparison/LineComparison.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.line_comparison;
+
+public class LineComparison {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation");
+	}
+}
