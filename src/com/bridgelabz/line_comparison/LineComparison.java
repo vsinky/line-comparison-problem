c5a0d46080f21a32cae87685d3919b0fd8b5c572
vsinky <sinkykumariverma@gmail.com>
2021-11-08 22:53:48 +0530

    check two line are equal to or not

--- src/com/bridgelabz/line_comparison/LineComparison.java
+++ src/com/bridgelabz/line_comparison/LineComparison.java
@@ -8,8 +8,20 @@ public class LineComparison {
 		int x2 = 7;
 		int y1 = 8;
 		int y2 = 89;
+		int p1 = 14;
+		int p2 = 16;
+		int q1 = 4;
+		int q2 = 85;
 
 		double lengthOfLine1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-		System.out.println("Length of Line : " + lengthOfLine1);
+		double lengthOfLine2 = Math.sqrt((p2 - p1) * (p2 - p1) + (q2 - q1) * (q2 - q1));
+		System.out.println("Length of Line1 : " + lengthOfLine1);
+		System.out.println("Length of Line2 : " + lengthOfLine2);
+
+		if (lengthOfLine1 == lengthOfLine2) {
+			System.out.println("Both lines are equal.");
+		} else {
+			System.out.println("Both lines are not equal.");
+		}
 	}
 }
